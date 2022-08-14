2eea5e236240fd89a1756fac8cad0e48a203efe0
tvallotton <tvallotton@uc.cl>
2022-08-14 13:57:03 -0400

    feat: write output to file

--- src/main.py
+++ src/main.py
@@ -13,18 +13,44 @@ def push_most_retweeted(current_top, new):
                 break
 
 
+
+
 def main(path):
 
     with open("dataset.json") as file:
         data = [json.loads(data) for data in file.readlines()]
 
     most_retweeted = []
-    
-
+    users = {}
+    days = {}
+    hashtags = {}
     for tweet in data: 
         push_most_retweeted(most_retweeted, tweet)
 
+        date = tweet["date"]
+        tweet_count = users.get(date, [0])[0]
+        days[date] = (tweet_count + 1, date)
+
+        username = tweet["user"]["username"]
+        tweet_count = users.get(username, [0])[0]
+        users[username] = (tweet_count + 1, tweet["user"])
+
+    users = list(users.values())
+    users.sort(key=lambda x: -x[0])
+    users = users[:10]
     
+    days = list(days.values())
+    days.sort(key=lambda x: -x[0])
+    days = days[:10]
+
+    with open("output.json", "w") as output: 
+        json.dumps({
+            "top_ten_days": days, 
+            "top_ten_users": users, 
+            "most_retweeted": most_retweeted,
+            "hashtags": hashtags
+        }, output)
+
 
 if __name__ == "__main__":
 
