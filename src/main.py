918f438c6f196a028b61962fdd559b0778d54a35
tvallotton <tvallotton@uc.cl>
2022-08-14 13:36:23 -0400

    feat: most retweeted tweets

--- src/main.py
+++ src/main.py
@@ -2,10 +2,29 @@ import sys
 import json
 
 
-def main(path):
-    with open(path) as file: 
-        data = json.load(file)
+def push_most_retweeted(current_top, new):
+    if len(current_top) < 10:
+        current_top.append(new)
+        
+    else: 
+        for i, tweet in enumerate(current_top):
+            if new["retweetCount"] > tweet["retweetCount"]:
+                current_top[i] = new
+                break
 
+
+def main(path):
+
+    with open("dataset.json") as file:
+        data = [json.loads(data) for data in file.readlines()]
+
+    most_retweeted = []
+    
+
+    for tweet in data: 
+        push_most_retweeted(most_retweeted, tweet)
+
+    
 
 if __name__ == "__main__":
 
